b35ca48b6ddbbee11cfea51e19218697260dbb8e
Fabian Oraze <oraze.fabian@icloud.com>
2020-03-07 19:48:09 +0100

    implemented connection to tcp server, it now reads my input and sends it to the server

--- app/src/main/java/com/example/einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispiel/MainActivity.java
@@ -3,14 +3,23 @@ package com.example.einzelbeispiel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText e1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        e1 = (EditText)findViewById(R.id.input_txt);
     }
-    
+
+    public void send(View v){
+        MessageSender messageSender = new MessageSender();
+        messageSender.execute(e1.getText().toString());
+    }
+
 }
