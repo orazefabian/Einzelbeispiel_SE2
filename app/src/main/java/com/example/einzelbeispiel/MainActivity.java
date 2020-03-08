04a00cc12393238bb8adf04ae37ce0278f45f936
Fabian Oraze <oraze.fabian@icloud.com>
2020-03-08 13:12:56 +0100

    added calc button and second output view for calculation, still todo: establish working connection to server

--- app/src/main/java/com/example/einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispiel/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -11,7 +12,10 @@ public class MainActivity extends AppCompatActivity {
 
     EditText input;
     TextView output;
-    TCP_Client client;
+    Button send;
+    Button calc;
+    TextView outputCalc;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,22 +23,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         input = (EditText) findViewById(R.id.input_txt);
         output = (TextView) findViewById(R.id.output_txt);
+        send = (Button) findViewById(R.id.btn_send);
+        calc = (Button) findViewById(R.id.btn_calc);
+        outputCalc = (TextView) findViewById(R.id.output_calc_text);
+
+        //create actions when send and calc button are pressed
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendToServer();
+            }
+        });
+        calc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculateOutput();
+            }
+        });
+
 
     }
 
+    private void calculateOutput() {
 
-
-    public void sendMessageToServer(View view) {
-
-        //get my input from the text field and send it to the server
         String matrikelnummer = input.getText().toString();
-        client = new TCP_Client();
-        client.setMatrikelnummer(matrikelnummer);
+        outputCalc.setText(Calculator.calculate_Common_factors(matrikelnummer));
+
+    }
+
+    private void sendToServer() {
+        TCP_Client client = new TCP_Client(input.getText().toString());
         client.start();
-
-        String returnMessage = client.getAnswerFromServer();
-
-        output.setText(returnMessage);
+        try {
+            client.join(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        output.setText(client.getAnswerFromServer());
 
     }
 
--- app/src/main/java/com/example/einzelbeispiel/TCP_Client.java
+++ app/src/main/java/com/example/einzelbeispiel/TCP_Client.java
@@ -1,33 +1,34 @@
 package com.example.einzelbeispiel;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class TCP_Client extends Thread {
 
 
-    String matrikelnummer;
-    String answerFromServer;
-    DataOutputStream out;
-    BufferedReader in;
+    private String matrikelnummer;
+    private String answerFromServer;
+    private DataOutputStream out;
+    private BufferedReader in;
+    private Socket socket = null;
 
+    public TCP_Client(String matrikelnummer){
+        this.matrikelnummer = matrikelnummer;
+    }
 
     @Override
     public void run() {
 
         try {
-            Socket socket = new Socket("se2-isys.aau.at", 53212);
+            socket = new Socket("se2-isys.aau.at", 53212);
             out = new DataOutputStream(socket.getOutputStream());
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
             out.writeBytes(matrikelnummer);
             answerFromServer = in.readLine();
-
             out.flush();
             in.close();
             socket.close();
@@ -40,14 +41,10 @@ public class TCP_Client extends Thread {
     }
 
     public String getAnswerFromServer() {
-        //calculate given task via the calculator class
-        answerFromServer = Calculator.calculate_Common_factors(matrikelnummer);
+
         return answerFromServer;
     }
 
-    public void setMatrikelnummer(String matrikelnummer) {
-        this.matrikelnummer = matrikelnummer;
-    }
 
 
 }
