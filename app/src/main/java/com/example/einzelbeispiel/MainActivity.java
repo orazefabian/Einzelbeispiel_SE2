ab0cbcc9807831b0095e928eb3eb2f9cae66b528
Fabian Oraze <oraze.fabian@icloud.com>
2020-03-08 11:40:41 +0100

    implemented TCP client, it reads a bytestream input calculates the task via the Calculator class and returns the message

--- app/src/main/java/com/example/einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispiel/MainActivity.java
@@ -11,16 +11,32 @@ public class MainActivity extends AppCompatActivity {
 
     EditText input;
     TextView output;
+    TCP_Client client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        input = (EditText)findViewById(R.id.input_txt);
-        output = (TextView)findViewById(R.id.output_txt);
+        input = (EditText) findViewById(R.id.input_txt);
+        output = (TextView) findViewById(R.id.output_txt);
+
     }
 
 
+
+    public void sendMessageToServer(View view) {
+
+        //get my input from the text field and send it to the server
+        String matrikelnummer = input.getText().toString();
+        client = new TCP_Client();
+        client.setMatrikelnummer(matrikelnummer);
+        client.start();
+
+        String returnMessage = client.getAnswerFromServer();
+
+        output.setText(returnMessage);
+
+    }
 
 
 }
--- app/src/main/java/com/example/einzelbeispiel/TCP_Client.java
+++ app/src/main/java/com/example/einzelbeispiel/TCP_Client.java
@@ -1,5 +1,53 @@
 package com.example.einzelbeispiel;
 
-public class TCP_Client {
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class TCP_Client extends Thread {
+
+
+    String matrikelnummer;
+    String answerFromServer;
+    DataOutputStream out;
+    BufferedReader in;
+
+
+    @Override
+    public void run() {
+
+        try {
+            Socket socket = new Socket("se2-isys.aau.at", 53212);
+            out = new DataOutputStream(socket.getOutputStream());
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            out.writeBytes(matrikelnummer);
+            answerFromServer = in.readLine();
+
+            out.flush();
+            in.close();
+            socket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public String getAnswerFromServer() {
+        //calculate given task via the calculator class
+        answerFromServer = Calculator.calculate_Common_factors(matrikelnummer);
+        return answerFromServer;
+    }
+
+    public void setMatrikelnummer(String matrikelnummer) {
+        this.matrikelnummer = matrikelnummer;
+    }
+
 
 }
